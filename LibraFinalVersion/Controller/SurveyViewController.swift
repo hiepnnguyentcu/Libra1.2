812f64bf9ecf53b5be378c31185531443cc6a87f
Hiep Nguyen <hiepnguyentpc@gmail.com>
2021-10-23 22:04:01 -0500

    survey running smoothly

--- LibraFinalVersion/Controller/SurveyViewController.swift
+++ LibraFinalVersion/Controller/SurveyViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class SurveyViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var notAtAllButton: UIButton!
+    @IBOutlet weak var severalDaysButton: UIButton!
+    @IBOutlet weak var moreThanHalfButton: UIButton!
+    @IBOutlet weak var nearlyEveryDayButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var surveyQuestionDataReal = surveyQuestionData()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func answerPressed(_ sender: UIButton) {
+        surveyQuestionDataReal.nextQuestion()
+        sender.backgroundColor = UIColor.green
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
-    */
+    
+    @objc func updateUI() {
+        questionLabel.text = surveyQuestionDataReal.getQuestionText()
+        progressBar.progress = surveyQuestionDataReal.getProgress()
+        notAtAllButton.backgroundColor = UIColor.clear
+        severalDaysButton.backgroundColor = UIColor.clear
+        moreThanHalfButton.backgroundColor = UIColor.clear
+        nearlyEveryDayButton.backgroundColor = UIColor.clear
+    }
+    
 
 }
