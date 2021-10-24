e2d3ea51ecc1369b30ccead3835b31bfc40fd275
Hiep Nguyen <hiepnguyentpc@gmail.com>
2021-10-24 02:43:12 -0500

    tableview added

---
+++ LibraFinalVersion/ZoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZoomViewController.swift
+//  LibraFinalVersion
+//
+//  Created by Hiệp Nguyễn on 24/10/2021.
+//
+
+import UIKit
+
+class ZoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
